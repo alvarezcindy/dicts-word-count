0789b82870c71e4de978fbf6ca8c00001fec2687
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-25 12:45:44 -0700

    Imported Counter to create dictionary

--- wordcount.py
+++ wordcount.py
@@ -1,17 +1,18 @@
 import sys
+from collections import Counter
 
 
 def count_words(filename):
 
-    word_count = {}
-
     with open(filename) as text:
-        for line in text:
-            words = line.split()
-            for word in words:
-                if word[-1] in "_.,?!;:\'\"":
-                    word = word[:-1]
-                word_count[word.lower()] = word_count.get(word, 0) + 1
+        words = text.read().split()
+
+        for i in range(len(words)):
+            words[i] = words[i].lower()
+            if words[i][-1] in "_.,?!;:\'\"":
+                words[i] = words[i][:-1]
+
+        word_count = Counter(words)
 
     for word, number in word_count.items():
         print("{} {}".format(word, number))
